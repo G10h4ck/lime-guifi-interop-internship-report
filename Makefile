7d0e0731461ebecadc775b0b54500e82ee65500e
Gioacchino Mazzurco <gio@eigenlab.org>
2015-09-07 11:45:14 +0200

    Render images at build time; Add anygw state of the art

--- Makefile
+++ Makefile
@@ -3,14 +3,11 @@ LUA_HIGHLIGHT_WORKAROUND_STYLE=lua-highlight-asciidoc-dblatex.sty
 ASCIIDOC_INSTALL_DIR="/usr/share/asciidoc/"
 
 .PHONY: all
-all: pdf html
-
-.PHONY: dia
-dia:
-	dia -O images -t png diagrams/*.dia
+all: html pdf
 
 .PHONY: pdf
-pdf: dia $(LUA_HIGHLIGHT_WORKAROUND_STYLE)
+pdf: $(LUA_HIGHLIGHT_WORKAROUND_STYLE)
+	./images.sh 300dpi
 	a2x --verbose -f pdf \
 		-a lang=en \
 		--asciidoc-opts="-f ./docbook-auto-references.conf" \
@@ -20,11 +17,15 @@ pdf: dia $(LUA_HIGHLIGHT_WORKAROUND_STYLE)
 		main.asciidoc
 
 .PHONY: html
-html: dia
+html:
+	./images.sh 820px
 	asciidoc --verbose \
 		-a toc2 \
 		main.asciidoc
 
+.PHONY: clean
+clean:
+	rm -rf images *.pdf *.html *.fo *.sty *.css
 
 # This is a work around to problem with asciidoc + dblatex and Lua syntax
 # highlighting inspired by suggestions on asciidoc and dblatex malinglists
---
+++ images.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+SIZE_OPT=""
+SUFFIX=""
+
+case ${1} in
+*dpi)
+	DPI="${1%%dpi}"
+	INKSCAPE_SIZE="--export-dpi ${DPI}"
+	SUFFIX="_${DPI}dpi.png"
+	;;
+*px)
+	WIDTH="${1%%px}"
+	INKSCAPE_SIZE="--export-width ${WIDTH}"
+	SUFFIX="_${WIDTH}px.png"
+esac
+
+mkdir -p images
+
+grep -h ^image:: *.asciidoc | while read IMAGE_LINE ; do
+	GRAPHNAME="${IMAGE_LINE%%_{*}"
+	GRAPHNAME="${GRAPHNAME##*/}"
+	FILE="$(ls graphics/${GRAPHNAME}.* | head -n 1)"
+	GRAPHTYPE="${FILE##*.}"
+
+	[ "${FILE}" -nt "images/${GRAPHNAME}${SUFFIX}" ] &&
+	{
+		case ${GRAPHTYPE} in
+		"dia")
+			dia -O images -t svg ${FILE}
+			inkscape --without-gui --export-area-drawing ${INKSCAPE_SIZE} \
+				--export-png=images/${GRAPHNAME}${SUFFIX} \
+				images/${GRAPHNAME}.svg
+			;;
+		"png")
+			cp ${FILE} images/${GRAPHNAME}${SUFFIX}
+			;;
+		"svg")
+			inkscape --without-gui --export-area-drawing ${INKSCAPE_SIZE} \
+				--export-png=images/${GRAPHNAME}${SUFFIX} \
+				graphics/${GRAPHNAME}.svg
+			;;
+		esac
+	}
+done
+
+exit 0
